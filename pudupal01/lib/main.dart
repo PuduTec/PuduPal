af3cbf728efb945cd8ae6fc27f204594b957ea2b
victor antonio morales alanis <vamorale@alumnos.inf.utfsm.cl>
2022-06-21 03:19:09 -0400

    Lista de páginas web y webview de cada una.

--- pudupal01/lib/main.dart
+++ pudupal01/lib/main.dart
@@ -1,115 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+class Pagina{
+  final String name;
+  final String url;
+
+  const Pagina(this.name, this.url);
 }
+//Lista de Páginas Web para trámites
 
+List<Pagina> _paginas = [
+  Pagina( "Clave Única", "https://claveunica.gob.cl/" ),
+  Pagina("Certificados Registro Civil", "https://www.registrocivil.cl/principal/servicios-en-linea"),
+  Pagina("AFP Provida","https://www.provida.cl/"),
+  Pagina("AFP Capital","https://home.afpcapital.cl/"),
+  Pagina("AFP Habitat","https://www.afphabitat.cl/"),
+  Pagina("AFP Modelo","https://nueva.afpmodelo.cl/afiliados"),
+  Pagina("AFP Plan Vital","https://www.planvital.cl/afiliado/inicio"),
+  Pagina("Pase Movilidad", "https://mevacuno.gob.cl/")
+];
+
+void main() => runApp(MyApp());
+
+//Muestra en pantalla la lista de páginas web
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final title = 'Trámites Web';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: _paginas.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+                title: Text(_paginas[index].name),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailScreen(pagina: _paginas[index]),
+                    ),
+                  );
+                }
+            );
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+//DetailScreen muestra la WebView al hacer click en un elemento de la lista
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key, required this.pagina});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final Pagina pagina;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(pagina.name),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: WebView(
+        initialUrl: pagina.url,
+        javascriptMode: JavascriptMode.unrestricted,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
