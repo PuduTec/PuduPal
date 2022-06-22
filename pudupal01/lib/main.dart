9dc1a0f8fb5851a3924653f79fdc6d0a6c753117
Alonso Zamorano <alonso.zamorano97@gmail.com>
2022-06-21 22:20:22 -0400

    Añadido comunicación entre webview y aplicación. Ahora se muestra la vista en la que uno esta

--- pudupal01/lib/main.dart
+++ pudupal01/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
 class Pagina {
   final String name;
@@ -55,46 +56,107 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//DetailScreen muestra la WebView al hacer click en un elemento de la lista
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key, required this.pagina});
 
   final Pagina pagina;
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+//DetailScreen muestra la WebView al hacer click en un elemento de la lista
+class _DetailScreenState extends State<DetailScreen> {
+  String instruction = "";
+  final controller = Completer<WebViewController>();
+  final js = '''
+function currentView() {
+    let url = window.location.host
+    if (url === "mevacuno.gob.cl") {
+        let popup_carnet = document.getElementsByClassName("popup-backdrop")[0]
+        if (popup_carnet.classList.contains("backdrop-in")) {
+            return "Inicio de Sesión Carnet"
+        }
+        else {
+            return "Hub Inicio de Sesión"
+        }
+    }
+    else if (url === "accounts.claveunica.gob.cl") {
+        return "Inicio de Sesión Clave Única"
+    }
+    else {
+        return "No se pude determinar la vista :("
+    }
+}
+
+window.addEventListener("touchstart", (e) => {
+    setTimeout(() => {
+        CurrentViewChannel.postMessage(currentView())
+    }, 400)
+})
+
+CurrentViewChannel.postMessage(currentView())
+''';
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(pagina.name),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: WebView(
-              initialUrl: pagina.url,
-              javascriptMode: JavascriptMode.unrestricted,
+    return FutureBuilder<WebViewController>(
+        future: controller.future,
+        builder: (context, futureController) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.pagina.name),
             ),
-          ),
-          Positioned(
-            bottom: 0.0,
-            left: 0.0,
-            right: 0.0,
-            child: Container(
-              height: 90,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: <BoxShadow>[
-                  BoxShadow(
-                    offset: const Offset(0.0, -0.5),
-                    color: Colors.black.withAlpha(30),
-                    blurRadius: 10.0,
+            body: Column(
+              children: [
+                Expanded(
+                  child: WebView(
+                    initialUrl: widget.pagina.url,
+                    javascriptMode: JavascriptMode.unrestricted,
+                    onWebViewCreated: (webViewController) {
+                      controller.complete(webViewController);
+                    },
+                    onPageFinished: (url) async {
+                      await futureController.data!.runJavascript(js);
+                    },
+                    javascriptChannels: _createJavascriptChannels(context),
                   ),
-                ],
-              ),
+                ),
+                Positioned(
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    height: 90,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          offset: const Offset(0.0, -0.5),
+                          color: Colors.black.withAlpha(30),
+                          blurRadius: 10.0,
+                        ),
+                      ],
+                    ),
+                    child: Expanded(child: Text(instruction)),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        });
+  }
+
+  Set<JavascriptChannel> _createJavascriptChannels(BuildContext context) {
+    return {
+      JavascriptChannel(
+        name: 'CurrentViewChannel',
+        onMessageReceived: (message) {
+          setState(() {
+            instruction = message.message;
+          });
+        },
       ),
-    );
+    };
   }
 }
